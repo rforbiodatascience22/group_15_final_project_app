f0698d09e714b483ffa8ab0240d66bfc8d7027d2
sorensandgaard <s173686@student.dtu.dk>
2022-05-04 08:02:38 +0000

    Employed PCA function in R/99

--- project_files/R/99_project_functions.R
+++ project_files/R/99_project_functions.R
@@ -93,3 +93,31 @@ dens_protein_BRCA <- function(data, proteins, attribute){
     theme_classic() +
     theme(legend.position = "bottom")
 }
+
+#### PCA ####
+pca_vis_BRCA <- function(data, PC1, PC2){
+  data <- my_data_clean_aug
+
+  data_wide <- data %>%
+    select(Age,matches("Protein"),Patient_Status_Binary) %>%
+    mutate(Patient_Status_Binary = case_when(Patient_Status_Binary == 0 ~ '0',
+                                             Patient_Status_Binary == 1 ~ '1')) %>%
+    mutate_at(c("Age","Protein1","Protein2","Protein3","Protein4"),
+              ~(scale(.) %>%  as.vector))
+
+  data_wide %>%
+    select(where(is.numeric)) %>%
+    prcomp(scale = TRUE) %>%
+    augment(data_wide) %>%
+    ggplot(aes(x = .fittedPC1,
+               y = .fittedPC2,
+               color = Patient_Status_Binary)) +
+    geom_point(size = 1.5) +
+    scale_color_discrete() +
+    theme_classic() +
+    background_grid()
+}
+
+
+
+
