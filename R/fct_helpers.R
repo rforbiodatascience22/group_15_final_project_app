7c723ee677617bb181e0626fdb344abc8b369b3a
mikkelswartz <s174586@student.dtu.dk>
2022-05-05 06:53:04 +0000

    fixed some errors when script was run twise

--- R/fct_helpers.R
+++ R/fct_helpers.R
@@ -17,19 +17,19 @@ filenames = c("R/01_load.R",
               "data/_raw/BRCA2.csv")
 
 # Download files from main repository
-dir.create("data")
-dir.create("data/_raw")
+dir.create(file.path("data", "data/_raw"), showWarnings = FALSE)
 for (filename in filenames) {
+
   file = read_file(str_c(path_from, filename))
   write_file(x=file,
             path = filename)
 }
 
 # Run files from main repository
-source(file = "R/01_load.R")
-source(file = "R/02_clean.R")
-source(file = "R/03_augment.R")
-source(file = "R/99_project_functions.R")
+#source(file = "R/01_load.R")
+#source(file = "R/02_clean.R")
+#source(file = "R/03_augment.R")
+#source(file = "R/99_project_functions.R")
 
 
 boldtext <- function (text)
